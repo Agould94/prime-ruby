d7de5daff2c65a2b66cbd8d51dc2a2d7f27e56c1
Alexander Gould <alexander.gould94@gmail.com>
2021-02-10 13:47:37 -0800

    commit tree b60e64ff177b89a81e146fe6e8acbe9b25e7c033

--- prime.rb
+++ prime.rb
@@ -1,1 +1,30 @@
-# Add  code here!
+require 'pry'
+def prime?(number)
+    if number < 2
+        false
+    elsif 
+        (2..(number-1)).any? do |p| 
+           if number % p == 0 
+            return false
+           end
+        end
+    else
+        true
+    end
+end
+
+
+    
+    
+    # p = (0..numbers)
+    # pp = p.to_a
+    # (pp.any?{|i| i})(pp.any?{|q| q}) == pp.any?{|i| i}
+
+    
+
+    # numbers.any? do |input|
+    #     (input).div(p.to_a.any?{|i| i}) == p.to_a.any?{|i| i}
+        # p.to_a.any? do |number|
+        #     q = (input).div(number)
+        #     q.any? {|i| i==number}
+
